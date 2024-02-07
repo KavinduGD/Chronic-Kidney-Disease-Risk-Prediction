ea99a0cb1a2120f5f7acceb495bf0e98e9e032f5
KavinduGD <it21038914@my.sliit.lk>
2024-02-07 17:12:20 +0530

    login page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,33 @@
 import React from "react";
-import img from "./3.jpg";
+import img from "./1.jpg";
+import { useNavigate } from "react-router-dom";
+
 function Home() {
+  const navigate = useNavigate();
   return (
     <div
-      className="h-screen bg-no-repeat bg-cover bg-right-bottom bg-fixed  sm:bg-top"
+      className="h-screen bg-no-repeat bg-cover bg-left-top flex flex-row-reverse "
       style={{ backgroundImage: `url(${img})` }}
     >
-      <p className="font-opensans text-6xl">sdsds</p>
+      <div
+        className="w-[550px] mr-[40px] mt-[50px] flex flex-col items-start "
+        style={{ lineHeight: 1.3 }}
+      >
+        <p
+          className="font-roboto text-[70px] font-extrabold text-red-700 "
+          style={{ textShadow: "1px 1px 3px red" }}
+        >
+          Chronic Kidney Disease Risk Prediction
+        </p>
+        <button
+          className="mt-[140px] py-[20px] px-[40px] text-[16px] font-poppins text-white bg-red-600 rounded-lg font-medium "
+          onClick={() => {
+            navigate("/form");
+          }}
+        >
+          Test the Kidney Health Risk Here
+        </button>
+      </div>
     </div>
   );
 }
