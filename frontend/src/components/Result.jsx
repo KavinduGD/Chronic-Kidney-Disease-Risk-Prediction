352b8097bc39b25849bf7d4f971b32f051a671ee
KavinduGD <it21038914@my.sliit.lk>
2024-02-08 11:45:24 +0530

    fdfd

--- frontend/src/components/Result.jsx
+++ frontend/src/components/Result.jsx
@@ -103,6 +103,7 @@ function Result({
     Fertilizer: [fertilizer],
   });
 
+  // console.log(result);
   return (
     <div className=" font-roboto">
       <div className="pr-[30px] bg-white pt-[14px] pb-[12px] pl-[20px] font-roboto border-[1px] border-[#DADCE0]  border-t-[10px] border-t-red-500 rounded-md ">
